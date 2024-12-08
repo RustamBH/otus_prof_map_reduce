776ab3db42ba2f167d376962d935b21721cb1282
RustamBH <kamaji@mail.ru>
2024-12-08 22:23:47 +0400

    Update mapper.cpp

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -13,14 +13,14 @@ int main(int argc, char ** argv)
 	std::size_t column = 9; // price
 
     // Map
-    std::vector<int> map_output;
+    //std::vector<int> map_output;
     while (std::getline(std::cin, line))
     {
         std::stringstream string_stream(line);
         int i = 0;
         while (std::getline(string_stream, item, delimiter)) {
             if (column == i) {
-                map_output.push_back(stoi(item));
+                //map_output.push_back(stoi(item));
                 std::cout << item << "\t" << "1" << std::endl;
                 break;
             }
