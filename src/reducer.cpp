7612332150cb1d5f366c0f667193aae9be463140
RustamBH <kamaji@mail.ru>
2024-12-18 23:48:40 +0400

    Update reducer.cpp

--- src/reducer.cpp
+++ src/reducer.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 
 
-double mean_eval(double& sum, const std::size_t& price, const std::size_t& count)
+double mean_eval(double& sum, const int& price, const std::size_t& count)
 {
 	sum += price;
 	return count > 0 ? sum / count : 0;    
@@ -27,11 +27,11 @@ int main(int argc, char ** argv)
 	double diff_sum = 0;
 	while (std::getline(std::cin, line))
 	{        
-		std::size_t price = std::stoi(line);
+		int price = std::stoi(line);
 		count++;
 		mean = mean_eval(sum, price, count);		
 		std::cout << "mean: " << mean << std::endl;
-		std::cout << "std_dev: " << stddev_eval(diff_sum, mean, price, count) << std::endl;
+		std::cout << "std_dev: " << stddev_eval(diff_sum, mean, price, count - 1) << std::endl;
 	}	
 	return 0;
 }
